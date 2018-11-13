c3eddff98c372ddf0fc0a109ed718dda6fc2bce3
Alejandro Gallo <aamsgallo@gmail.com>
2018-11-14 00:53:54 +0100

    Clean up yaml

--- src/yaml.rs
+++ src/yaml.rs
@@ -7,7 +7,5 @@ pub fn from_file(file_name: &String) -> vec::Vec<yaml_rust::Yaml> {
     let mut fd = fs::File::open(&file_name).expect("Unable to open file");
     let mut data = String::new();
     fd.read_to_string(&mut data).expect("Unable to read file");
-    println!("{}", data);
-    let docs = yaml_rust::YamlLoader::load_from_str(&data).unwrap();
-    docs
+    yaml_rust::YamlLoader::load_from_str(&data).unwrap()
 }
