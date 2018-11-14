33b86aee050713445def16c3589e910b4635359b
Alejandro Gallo <aamsgallo@gmail.com>
2018-11-14 01:46:17 +0100

    Add clap library

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use std::env;
+extern crate clap;
+
+use clap::{Arg, App};
 
 mod yaml;
 
 fn main() {
+    let matches = App::new("rquizis")
+        .version("0.1.0")
+        .arg(
+            Arg::with_name("file")
+            .takes_value(true)
+        )
+        .get_matches();
 
-    let args: Vec<_> = env::args().collect();
-    if args.len() == 1 {
-        println!("No arguments given, exiting...");
-        std::process::exit(1);
-    }
-    let docs = yaml::from_file(&args[1]);
+    let input_file = matches.value_of("file").unwrap().to_string();
+    let docs = yaml::from_file(&input_file);
     println!("{:?}", docs);
     println!("\n\n{}", docs[0][2]["key"].as_str().unwrap());
 
